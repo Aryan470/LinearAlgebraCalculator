fe34f95cc477035d5b7077b25defc7a26eb92da3
Aryan470 <aryankhatri2012@gmail.com>
2020-06-02 03:11:21 +0000

    Added basic JSON parsing to client.js

--- target/classes/public/client.js
+++ target/classes/public/client.js
@@ -10,8 +10,30 @@ function processExpression() {
     xhr.open("POST", url, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
-            document.getElementById("output").innerHTML = "Result: " + xhr.responseText;
+            processResults(xhr.responseText);
         }
     };
     xhr.send(data);
+}
+
+function processResults(responseText) {
+    let response = JSON.parse(responseText);
+    const type = response["@type"];
+    let result = "NO RESULT";
+    if (type == "Report") {
+        if (response.message != null) {
+            result = name + "<br>" + response.message;
+        } else {
+            result = name + "<br>" + response.content;
+        }
+    } else if (type == "Scalar") {
+        result = response.value;
+
+    } else if (type == "Vector") {
+        result = response.components;
+        
+    } else if (type == "Matrix") {
+        result = response.components;
+    }
+    document.getElementById("output").innerHTML = result;
 }
